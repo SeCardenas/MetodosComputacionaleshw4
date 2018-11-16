ae9e9b521ca024b8d814782090a187d9a81af9c2
secardenas <se.cardenas@uniandes.edu.co>
2018-11-16 15:36:34 -0500

    algunas correcciones

--- CardenasSergio_hw4/ODE.cpp
+++ CardenasSergio_hw4/ODE.cpp
@@ -7,21 +7,23 @@ using namespace std;
 const int g = 10;
 const double c = 0.2, m = 0.2;
 
-vector<double> derivative1(vector<double> x, vector<double> v);
-vector<double> derivative2(vector<double> x, vector<double> v);
+vector<double> derivativeX(vector<double> x, vector<double> v);
+vector<double> derivativeV(vector<double> x, vector<double> v);
 
 int main() {
 	double dt = 0.01;
 }
 
-vector<double> derivative1(vector<double> x, vector<double> v) {
+vector<double> derivativeX(vector<double> x, vector<double> v) {
 	vector<double> deriv(2);
 	deriv[0] = v[0];
 	deriv[1] = v[1];
+	return deriv;
 }
 
-vector<double> derivative2(vector<double> x, vector<double> v) {
+vector<double> derivativeV(vector<double> x, vector<double> v) {
 	vector<double> deriv(2);
 	deriv[0] = -c/m * sqrt((v[0]*v[0] + v[1]*v[1]))*v[0]; //dv/dt en x
 	deriv[1] = -g-c/m * sqrt((v[0]*v[0] + v[1]*v[1]))*v[1]; //dv/dt en y
+	return deriv;
 }
