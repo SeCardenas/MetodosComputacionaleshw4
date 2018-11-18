b9cbf53a0993948836b762c935d072681eaacb7e
secardenas <se.cardenas@uniandes.edu.co>
2018-11-18 09:16:46 -0500

    Punto 3 empezado, definidas las constantes

---
+++ CardenasSergio_hw4/PDE.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+
+const double k = 1.62;
+const double Cp = 820;
+const double rho = 2.71;
+const double nu = k/(Cp*rho);
+
+const double diametro = 0.1; // en metros
+const double L = 0.5; // en metros
+const double T0_c = 10, T0_b = 50; //°C
+
+
+using namespace std;
+
+int main() {
+	
+}
